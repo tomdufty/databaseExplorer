faedbdb97634ff1fe4528ea498fc422f2316df0f
tomdufty <tom.dufty@hotmail.com>
2018-05-11 15:44:07 +1000

    initial search query setup and improt began

--- src/main.py
+++ src/main.py
@@ -5,19 +5,37 @@ import mainWindow # This file holds our MainWindow and all design related things
               # it also keeps events etc that we defined in Qt Designer
 import dbmanager
 
-class ExampleApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
+class Application(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
+    #
     def __init__(self):
 
         super(self.__class__, self).__init__()
         self.setupUi(self)  # This is defined in design.py file automatically
                             # It sets up layout and widgets that are defined
+        self.actionFrom_CSV.triggered.connect(self.file_open)
 
+    def run_search_query(self):
+        searchquery = dbmanager.run_search_query(self.textEdit.toPlainText())
 
+    def run_import_from_csv(self):
+        print("select file to import")
+
+    def file_open(self):
+        name = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File')
+        file = open(name,'r')
+
+        self.editor()
+
+        with file:
+            text = file.read()
+            self.textEdit.setText(text)
+
+# main body of program initialised on startup
 def main():
     entrys = dbmanager.get_all_descriptions()        #initial test of connection - requires more error handling
     print(entrys)
     app = QtWidgets.QApplication(sys.argv)  # A new instance of QApplication
-    form = ExampleApp()                 # We set the form to be our ExampleApp (design)
+    form = Application()                 # We set the form to be our ExampleApp (design)
           # and execute the app
     for entry in entrys:
         form.MainList.insertRow(0)
